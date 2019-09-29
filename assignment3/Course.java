b6b4ee0fab85928a96375c773c132c62d3d80764
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-09-28 20:50:01 -0700

    Create Course.java

---
+++ assignment3/Course.java
@@ -0,0 +1,20 @@
+/* 
+Q1. Write a Java class named 'Course'
+It should have following members
+courseId
+courseName
+maxCapacity
+professorId
+credits
+int[] studentIds
+
+Function: registerStudent(int studentId)
+
+There should be 3 ways to create Course Object (Hint : Use Constructor)
+courseId
+courseId, professorId
+courseId, professorId, credits
+
+Class should provide getters/setters for all members
+registerStudent method should store all ids in studentIds array.
+*/
