258a2b3d26f15a251dbe14822f84f629fa99268a
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-09-28 21:32:59 -0700

    Update Course.java

--- assignment3/Course.java
+++ assignment3/Course.java
@@ -18,3 +18,64 @@ courseId, professorId, credits
 Class should provide getters/setters for all members
 registerStudent method should store all ids in studentIds array.
 */
+public class Course {
+  private int courseId, professorId;
+  private String courseName;
+  private int maxCapcity;
+  private float credits;
+  private ArrayList<Integer> studentIds = new ArrayList<Integer>();
+  
+  
+  Course(int courseId) {
+    this.courseId = courseId;
+  }
+  
+  Course(int courseId, int professorId) {
+    this.courseId = courseId;
+    this.professorId = prodessorId;
+  }
+  
+  Course(int courseId, int professorId, float credits) {
+    this.courseId = courseId;
+    this.professorId = prodessorId;
+    this.credits = credits;
+  }
+  
+  public void setCourseId(int courseId) {
+    this.courseId = courseId;
+  }
+  public int getCourseId() {
+    return courseId;
+  }
+  public void setProfessorId(int professorId) {
+    this.professorId = professorId;
+  }
+  public int getProffesorId() {
+    return professorId;
+  }
+  public void setCourseName(String courseName) {
+    this.courseName = courseName;
+  }
+  public String getCourseName() {
+    return courseName;
+  }
+  public void setMaxCapacity(int maxCapacity) {
+    this.maxCapacity = maxCapacity;
+  }
+  public int getMaxCapacity() {
+    return maxCapacity;
+  }
+  public void setCredits(float credits) {
+    this.credits = credits;
+  }
+  public float getCredits() {
+    return credits;
+  }
+
+  
+  public void registerStudent(int studentId) {
+    this.studentIds.add(studentId);
+  }
+ 
+  
+  
