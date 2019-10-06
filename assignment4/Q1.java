fb2f97c5a8cdb0ea6af4a576b9e1292bd9afd66b
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-06 13:46:34 -0700

    Update Q1.java

--- assignment4/Q1.java
+++ assignment4/Q1.java
@@ -1,1 +1,38 @@
 
+public abstract class Shape {
+  String name;
+  double perimeter;
+  double area;
+  
+//   Shape(String name, double perimeter, double area) {
+//     this.name = name;
+//     this.perimeter = perimeter;
+//     this.area = area;
+//   }
+  
+  void draw() {
+    System.out.pirntIn("Drawing shape");
+  };
+  abstract getArea();
+  abstract getPerimeter();
+  
+}
+  
+
+
+public class Circle extends Shape {
+  
+  
+  void draw() {
+    System.out.printIn("Drawing circle");
+  }
+  
+  public double getArea(double radius) {
+    area = 3.14 * radius * radius;
+    return area;
+  }
+  public double getPerimeter(double radius) {
+    perimeter = 2 * 3.14 * radius;
+    return perimeter;
+  }
+}
