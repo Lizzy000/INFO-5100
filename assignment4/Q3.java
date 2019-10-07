f1b1d6da0aeda327d5ee18b383fc25e43cfb8d4a
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 00:35:47 -0700

    Update Q3.java

--- assignment4/Q3.java
+++ assignment4/Q3.java
@@ -1,1 +1,9 @@
+abstract class DessertItem {
+  String name;
+}
 
+class Candy extends DessertItem {}
+class Cookie extends DessertItem {}
+class IceCream extends DessertItem {}
+class Sandae extends IceCream {}
+class CheckOut {}
