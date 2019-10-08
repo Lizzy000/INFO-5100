59400a33a882925b25e22f7e20fe29150d160627
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:40:44 -0700

    Update Candy.java

--- assignment4/Q3/Candy.java
+++ assignment4/Q3/Candy.java
@@ -2,7 +2,7 @@ public class Candy extends DessertItem{
   
   private double weight;
   private double price;
-  private double cost = weight * price;
+  private double cost = Math.round(weight * price);
   
   Candy(String name, double weight, double price) {
     super.name = name;
