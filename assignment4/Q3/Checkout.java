241c63f2f588b978563f2769d06671cc4d61a7b7
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 22:48:22 -0700

    Update Checkout.java

--- assignment4/Q3/Checkout.java
+++ assignment4/Q3/Checkout.java
@@ -1,5 +1,21 @@
-public class Checkout {
+import java.util.Iterator;
+import java.util.Vector;
+
+public class Checkout extends Vector {
   Checkout() {
+    super();
+  }
   
+  public void enterItem(DessertItem item) {
+    super.addElement(item);
+  }
+  
+  public void clear() {
+    super.clear();
+  }
+  
+  public int totalCost() {
+  
+    
   }
 }
