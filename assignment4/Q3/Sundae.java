07ee27b95786e4750e60ce1d84cfa39a8f02bd67
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:10:41 -0700

    Update Sundae.java

--- assignment4/Q3/Sundae.java
+++ assignment4/Q3/Sundae.java
@@ -1,1 +1,10 @@
-
+public class Sundae extends IceCream {
+  private String toppingName;
+  private double toppingPrice;
+  
+  Sundae(String name, double price, String toppingName, double toppingPirce) {
+    super(name, price);
+    this.toppingName = toppingName;
+    this.toppingPrice = topingPrice/100;
+  }
+}
