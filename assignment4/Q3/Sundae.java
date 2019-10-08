5fff2a41f71d31149044324e76d037830a3b89a8
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:42:04 -0700

    Update Sundae.java

--- assignment4/Q3/Sundae.java
+++ assignment4/Q3/Sundae.java
@@ -1,11 +1,11 @@
 public class Sundae extends IceCream {
   private String toppingName;
   private double toppingPrice;
-  private double cost = super.cost + toppingPrice;
+  private double cost = Math.round(super.cost + toppingPrice);
   
   Sundae(String name, double cost, String toppingName, double toppingPirce) {
     super(name, cost);
     this.toppingName = toppingName;
-    this.toppingPrice = topingPrice/100;
+    this.toppingPrice = Math.round(topingPrice/100);
   }
 }
