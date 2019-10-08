9e8c535abf188124107cef602486e3a0b7919aef
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:04:53 -0700

    Update Candy.java

--- assignment4/Q3/Candy.java
+++ assignment4/Q3/Candy.java
@@ -1,1 +1,11 @@
-
+public class Candy extends DessertItem{
+  
+  private double weight;
+  private double price;
+  
+  Candy(String name, double weight, double priceCent) {
+    super.name = name;
+    this.weight = weight;
+    this.price = priceCent/100;
+  }
+}
