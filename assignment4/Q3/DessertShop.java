af0e116df9a8342c7dbe661fbca6cfcdf3739f80
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-08 00:09:11 -0700

    Update DessertShop.java

--- assignment4/Q3/DessertShop.java
+++ assignment4/Q3/DessertShop.java
@@ -1,1 +1,11 @@
-
+public class DessertShop {
+  public static final float TAXRATE = 0.002f;
+  public static final String STORENAME = "M & M Dessert Shop";
+  public static final int WIDTH = 10;
+  
+  public String cents2dollarsAndCents(int cents) {
+    String result = toString(cents/100);
+    return 
+  }
+  
+}
