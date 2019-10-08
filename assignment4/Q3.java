5ef535b0276f755eff5552e10c505c9ac44dc470
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-08 11:33:41 -0700

    Delete Q3.java

--- assignment4/Q3.java
+++
@@ -1,9 +0,0 @@
-abstract class DessertItem {
-  String name;
-}
-
-class Candy extends DessertItem {}
-class Cookie extends DessertItem {}
-class IceCream extends DessertItem {}
-class Sandae extends IceCream {}
-class CheckOut {}
