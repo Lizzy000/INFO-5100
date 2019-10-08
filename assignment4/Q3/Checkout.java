5518d347c140a57c56c502f798a9828ae1fc63c4
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-08 02:20:46 -0700

    Update Checkout.java

--- assignment4/Q3/Checkout.java
+++ assignment4/Q3/Checkout.java
@@ -1,21 +1,39 @@
-import java.util.Iterator;
+
 import java.util.Vector;
 
 public class Checkout extends Vector {
+  int totalCost = 0;
+  double totalTax = 0;
+  int count = 0;
+  
   Checkout() {
     super();
+    DessertShop DS = new DessertShop();
+    addElement(STORENAME);
+    addElement(DIVIDER);
   }
   
   public void enterItem(DessertItem item) {
-    super.addElement(item);
+    addElement(item);
+    totalCost += item.cost;
+    count++;
   }
   
   public void clear() {
-    super.clear();
+    clear();
   }
   
   public int totalCost() {
+    return totalCost;
+  }
   
+  public double totalTax() {
     
+    totalTax = totalCost * TAXRATE;
+    return totalTax;
+  }
+  
+  public int numberOfItems() {  
+    return count ;
   }
 }
