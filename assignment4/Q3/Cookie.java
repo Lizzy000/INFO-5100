c1089a3314fbf6a63686647d0b71f21043082837
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:24:44 -0700

    Update Cookie.java

--- assignment4/Q3/Cookie.java
+++ assignment4/Q3/Cookie.java
@@ -3,4 +3,10 @@ public class Cookies extends DessertItem {
   private double price;
   private double cost = (number/12) * price;
   
+  Cookies(String name, int number, double price) {
+    super(name);
+    this.number = number;
+    this.price = price/100;
+  }
+  
 }
