236f3a849ac21f4c0f7243a1c8f4d82f60746bfc
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-08 01:41:45 -0700

    Update DessertShop.java

--- assignment4/Q3/DessertShop.java
+++ assignment4/Q3/DessertShop.java
@@ -3,7 +3,7 @@ public class DessertShop {
   public static final String STORENAME = "M & M Dessert Shop";
   public static final int WIDTH = 2;
   public static final int MAXSIZENAME = 50;
-  public static final String partion = "-------------------";
+  public static final String DIVIDER = "-------------------";
   
   public String cents2dollarsAndCents(int cents) {
     return String.valueOf(cents/100);
