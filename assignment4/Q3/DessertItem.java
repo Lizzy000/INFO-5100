3eacc1f0142a9f89986378a3a7a3368e41a3a787
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-07 23:38:38 -0700

    Update DessertItem.java

--- assignment4/Q3/DessertItem.java
+++ assignment4/Q3/DessertItem.java
@@ -1,7 +1,16 @@
 abstract public class DessertItem {
   public String name;
   
-  DessertItem(String name) {
+  public DessertItem() {
+  }
+  
+  public DessertItem(String name) {
     this.name = name;
   }
+  
+  public final String getName() {
+    return this.name;
+  }
+  
+  abstract double getCost();
 }
