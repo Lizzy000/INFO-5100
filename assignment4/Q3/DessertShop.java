f18cc452160d3e0208331e5bd34d63e08a79f49d
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-08 01:58:15 -0700

    Update DessertShop.java

--- assignment4/Q3/DessertShop.java
+++ assignment4/Q3/DessertShop.java
@@ -2,7 +2,7 @@ public class DessertShop {
   public static final float TAXRATE = 0.002f;
   public static final String STORENAME = "M & M Dessert Shop";
   public static final int WIDTH = 2;
-  public static final int MAXSIZENAME = 50;
+  public static final int MAXNAME = 50;
   public static final String DIVIDER = "-------------------";
   
   public String cents2dollarsAndCents(int cents) {
