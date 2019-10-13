3f83557ae9a91ba882f405e412c3740fbdd09fd2
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-13 00:40:50 -0700

    Update Number.java

--- assignment5/Number.java
+++ assignment5/Number.java
@@ -1,6 +1,9 @@
 public class Number {
   int count(int a, int b) {
-    try(int q = a/b)
-      catch()
+    try {
+      int q = a/b;
+    } catch (ArithmeticException e1) {
+      System.out.printIn("Division by 0");
+    }
   }
 }
