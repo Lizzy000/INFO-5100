841c46cab500de2136a7002269549af48ebb8637
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-13 01:01:35 -0700

    Update FullTime.java

--- assignment5/FullTime.java
+++ assignment5/FullTime.java
@@ -1,1 +1,9 @@
-
+class FullTime extends Employee {
+  public FullTime(String name, int paymentPerHour) {
+    super(name, paymentPerHour);
+  }
+  public int calculateSalary() {
+    int salary = 8 * this.paymentPerHour;
+    return salary;
+  }
+}
