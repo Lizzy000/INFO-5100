21a5fb25064cb0b91f7297995a3d47e90736964b
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-12 20:45:16 -0700

    Update Q5.java

--- assignment4/Q5.java
+++ assignment4/Q5.java
@@ -1,1 +1,25 @@
+public class Solution {
+  public List<Integer> solution(String s) {
+    List<Integer> list = new ArrayList<Integer>();
+    int in = 0;
+    int de = s.length();
+    for (int i = 0; i <= s.length() ; i++) {
+	    if (s.charAt(i) == "I") {
+		    list.add(in);
+		    in++;
+	    } 
+	    if (s.charAt(i)== "D") {
+		    list.add(de);
+		    de--;
+	    }
+    }
+    if (s.charAt(s.length()) == "I") {
+	    list.add(in);
+    }
 
+    if (s.charAt(s.lenght()) == "D") {
+	    list.add(de);
+    }
+    return list;
+  }
+}
