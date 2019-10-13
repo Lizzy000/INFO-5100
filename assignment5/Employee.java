efcc4e438d63313a76a281e2edb35c4d20ff0d33
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-13 00:51:33 -0700

    Update Employee.java

--- assignment5/Employee.java
+++ assignment5/Employee.java
@@ -1,1 +1,25 @@
-
+abstract class Employee {
+  private String name;
+  private int paymentPerHour;
+  
+  public Employee(String name, int paymentPerHour) {
+    this.name = name;
+    this.paymentPerHour = paymentPerHour;
+  }
+  
+  public abstract int calculateSalary();
+  public String getName() {
+    String name = this.name;
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public int getPaymentPerHour() {
+    int paymentPerHour = this.paymentPerHour;
+    return paymentPerHour;
+  }
+  public void setPaymentPerHour(int paymentPerHour) {
+    this.paymentPerHour = paymentPerHour;
+  }
+}
