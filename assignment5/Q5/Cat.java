33f19c3d23586e99312305474da1f40e08a5dfec
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-24 19:33:24 -0700

    Update Cat.java

--- assignment5/Q5/Cat.java
+++ assignment5/Q5/Cat.java
@@ -1,1 +1,19 @@
+import pet;
+import boardable;
 
+class Cat extends Pet implements Boardable {
+  private String hairLength;
+  
+  Cat (String name, String ownerName, String color, String hairLength) {
+    super(name, ownerName, color);
+    this.hairLength = hairLength;
+  }
+  
+  public String getHairLength() {
+    return this.hairLength;
+  }
+  
+  public String toString() {
+  
+  }
+}
