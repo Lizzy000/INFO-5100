12da2a998353b2065f7a8720e3b898281716061c
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-24 19:20:50 -0700

    Update Pet.java

--- assignment5/Q5/Pet.java
+++ assignment5/Q5/Pet.java
@@ -1,1 +1,35 @@
+package pet;
 
+public class Pet {
+  private String petName;
+  private String ownerName;
+  private String color;
+  protected int sex;
+  static final int MALE = 0;
+  static final int FEMALE = 1;
+  static final int SPAYED = 2;
+  static final int NEUTERED = 3;
+  
+  Pet (String name, String ownerName, String color) {
+    this.petName = name;
+    this.ownerName = ownername;
+    this.color = color;
+  }
+  
+  public String getPetName() {
+    return this.petName;
+  } 
+  public String getOwnerName() {
+    return this.ownerName;
+  }
+  public String getColor() {
+    return this.color;
+  } 
+  
+  public void setSex(int sexid) {
+    this.sex = sexid;
+  }
+  public String toString() {
+    return petName + " owned by " + ownerName + "\n" + "Color:" + color + "\n" + "Sex:" + sex;
+  }
+}
