a7968a1618e9568972129047ef39fa62627d48c6
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-24 19:25:12 -0700

    Update Boardable.java

--- assignment5/Q5/Boardable.java
+++ assignment5/Q5/Boardable.java
@@ -1,1 +1,7 @@
+package boardable;
 
+public interface Boardable {
+  void setBoardStart(int month, int day, int year); 
+  void setBoardEnd(int month, int day, int year); 
+  boolean boarding(int month, int day, int year); 
+}
