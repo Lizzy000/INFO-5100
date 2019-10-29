a039956b1e81dd0b7c9abcd15c8c27d3a142ad1d
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-29 01:28:02 -0700

    Update Q4.java

--- assignment6/Q4.java
+++ assignment6/Q4.java
@@ -1,1 +1,16 @@
 
+public boolean uniqueOccurrences(int[] arr) {
+  Map<Integer, Integer> occ = new HashMap<>();
+  for(int i = 0; i < arr.length; i++) {
+    int count = 0;
+    if(occ.get(arr[i]) != null) {
+      count = occ.get(arr[i]);
+    }
+    occ.put(arr[i], count++);
+  }
+  for(Integer key: occ.keyset()) {
+    int value = occ.get(key);
+    if(occ.containsValue(value)) return false;
+  }
+  return true;
+}
