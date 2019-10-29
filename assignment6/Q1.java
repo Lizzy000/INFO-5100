ab83bc09346e47572b9fb62e27b84d37ccf2f6c5
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-28 23:56:53 -0700

    Update Q1.java

--- assignment6/Q1.java
+++ assignment6/Q1.java
@@ -1,1 +1,26 @@
+public class User {
+  String name;
+  int age;
+  String address;
+  int phoneNumber;
+  int bankAccountNumber;
+  
+  User(String name, int age, String address, int phoneNumber, int bankAccountNumber) {
+    this.name = name;
+    this.age = age;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+    this.bankAccountNumber = bankAccountNumber;
+  }
+}
 
+public class ATM {
+  int availableAmountInMachine;
+  int transactionFee;
+  Map<Object, String> userData = new HashMap<>();
+  
+  ATM(int availableAmountInMachine, int transactionFee) {
+    this.availableAmountInMachine = availableAmountInMachine;
+    this.transactionFee = transactionFee;
+  }
+}
