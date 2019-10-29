649bad8caf0a63ab5869739ab8184fa11dc6733e
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-29 01:34:28 -0700

    Update Q5.java

--- assignment6/Q5.java
+++ assignment6/Q5.java
@@ -1,1 +1,10 @@
-
+public class MyHashMap {
+  
+  List<Object> array = new ArrayList<>();
+  //List<Object> list = new LinkedList<>();
+  
+  
+  public void put(Object, Object) {}
+  
+  public Object get() {}
+}
