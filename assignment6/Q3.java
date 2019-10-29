2df32cf303fe4a47d931aeb63a8c08053ffb982f
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-10-29 00:28:32 -0700

    Update Q3.java

--- assignment6/Q3.java
+++ assignment6/Q3.java
@@ -1,1 +1,31 @@
-
+class Solution {
+  public boolean isAnagram(String s, String t) {
+    
+    if(s.length() != t.length()) return false;
+    
+    char[] S = s.toCharArray();
+    char[] T = t.toCharArray();
+    
+    List<char> Ss = new ArrayList<>();
+    List<char> Tt = new ArrayList<>();
+    
+    for(int i = 0; i < S.length; i++) {
+      Ss.add(S[i]);
+    }
+    for(int i = 0; i < T.length; i++) {
+      Tt.add(T[i]);
+    }
+    
+    while(Ss.size() != 0) {
+      for(int i = 0; i < Ss.size(); i++) {
+        if(Ss.get(i) == Tt.get(0)) {
+          Ss.remove(i);
+          Tt.remove(0);
+          break;
+        }
+      }
+      return false;
+    }
+    
+  }
+}
