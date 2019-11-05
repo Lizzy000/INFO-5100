0c9ad0ab33d1bfdf8818f9589c06745ac10764dc
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-11-04 22:18:23 -0800

    Update test.java

--- assignment7/Q1/test.java
+++ assignment7/Q1/test.java
@@ -1,1 +1,15 @@
+import java.io.*
 
+public class test {
+  public static void main(String args[]) {
+    FileInputStream fis = new FileInputStream(...);
+    FileOutputStream fos = new FileOutputStream(...);
+    
+    StringBuffer sb = new StringBuffer();
+    int tempBit;
+    while((tempBit = fis.read()) != -1) {
+      sb.append(tempBit);
+    }
+    
+  }
+}
