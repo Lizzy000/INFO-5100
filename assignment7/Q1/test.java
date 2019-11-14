50837653c4f9764bccd167bb967329f2cdd3abf6
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-11-14 11:41:10 -0800

    Update test.java

--- assignment7/Q1/test.java
+++ assignment7/Q1/test.java
@@ -1,16 +1,36 @@
 import java.io.*
 
-public class test {
-  public static void main(String args[]) {
-    FileInputStream fis = new FileInputStream(...);
-    FileOutputStream fos = new FileOutputStream(...);
-    
-    StringBuffer sb = new StringBuffer();
-    int tempBit;
-    while((tempBit = fis.read()) != -1) {
-      sb.append((char)tempBit);
-    }
-    fis.close();
-    
-  }
+  
+public static void main(String[] args) throws Exception {
+		File input = new File("INFO-5100/assignment7/Q1/Input.txt");
+		FileInputStream fis = new FileInputStream(input);
+		String result = readFromStream(fis);
+  
+    String s[] = result.split(" "); 
+    String ans = ""; 
+    for (int i = s.length - 1; i >= 0; i--) { 
+      ans += s[i] + " "; 
+    } 
+    writeToFile(ans);
+}
+
+private String readFromStream(FileInputStream fis) throws IOException {
+    String result = "";
+		while (true) {
+			int x = fis.read();
+			if (x == -1)
+				break;
+			char c = (char) x;
+			result += c;
+		}
+		fis.close();
+    return result;
+}
+
+
+private void writeToFile(String ans) throws IOException {
+    File file = new File("INFO-5100/assignment7/Q1/Output.txt");
+		FileOutputStream fos = new FileOutputStream(file);
+		fos.write(ans.getBytes());
+		fos.close();
 }
