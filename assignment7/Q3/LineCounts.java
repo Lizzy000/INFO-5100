ab4f30547d102035fbe5f430206d619080239bd2
Lizzy000 <55042806+Lizzy000@users.noreply.github.com>
2019-11-15 00:28:55 -0800

    Update LineCounts.java

--- assignment7/Q3/LineCounts.java
+++ assignment7/Q3/LineCounts.java
@@ -1,1 +1,25 @@
+public class LineCounts {
 
+  public static void main(String[] args) throws IOException {
+    String s = "file1.txt file2.txt file3.txt";
+    String[] fileName = s.split("");
+    
+    for(int i = 0; i < fileName.length(); i++) {
+      try {
+         File input = new File("/INFO-5100/assignment7/Q3/" + fileName[i]);
+         Scanner temp = new Scanner(input);
+         int count = 0;
+         while(temp.hasNextLine()) count++;
+         File output = new File("INFO-5100/assignment7/Q3/output.txt");
+         FileOutputStream fos = new FileOutputStream(output);
+        
+         String result = fileName[i] + "has" + count + "line(s)" + "/n";
+         fos.write(result.getBytes());
+	       fos.close();
+      } catch (IOException ioe) {
+         System.printIn.out(fileName[i] + "does not exist.");
+      }
+      
+    }
+  }
+}
